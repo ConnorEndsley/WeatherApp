b96bfe1e472862e1dc969cd44ac28367fd6faefd
ConnorEndsley <endsleyconnor95@gmail.com>
2022-07-25 21:57:47 -0500

    styled page, responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const time = document.getElementById("time")
+const date = document.getElementById("date")
+const currentWeatherItems = document.getElementById('current-weather-items');
+const timeZone = document.getElementById('time-zone');
+const country = document.getElementById('country');
+const weatherForecast = docuemnt.getElementById('weather-forecast')
+
